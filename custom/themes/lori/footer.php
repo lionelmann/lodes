021aa4e79168a2ecd5a12662b90b15dd4a994829
Lionel Mann <lionelmann@gmail.com>
2016-06-27 14:15:03 -0400

    footer address

--- custom/themes/lori/footer.php
+++ custom/themes/lori/footer.php
@@ -1,16 +1,25 @@
 <footer>
     <div class="inner">
-    <article class='center'>
-        <div class="columns-2">
+    <article class="semi-wide">
+
+        <div class="columns-3" style="text-align: left;">
+        Desjardine Rehabilitation<br>
+        1500 Upper Middle Rd. Unit #3<br>
+        PO Box 76072 Abbey Market<br>
+        Oakville ON L6M3H5<br><br>
+        <b>tel:</b> 416 648 7322<br>
+        <b>fax:</b> 289-815-0639
+        </div>
+        <div class="columns-3">
             <a href="http://www.brainfx.com/" target="_blank"><img src="<?php bloginfo('template_url' ); ?>/dist/images/brainseal_admin.png"></a>
         </div>
-        <div class="columns-2">
+        <div class="columns-3">
             <a href="http://www.cogmed.com/" target="_blank"><img src="<?php bloginfo('template_url' ); ?>/dist/images/cogmed-shield.png"></a>
         </div>
+
         </article>
     </div>
 
-    
     <div class="footer__copyright">
         <div class="inner">
             <small>&#169; Copyright | <a href="<?php bloginfo('url'); ?>/privacy-policy">Privacy Policy</a></small>
