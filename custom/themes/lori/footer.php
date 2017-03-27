373dcc9583c794ac161a559afd83dd0d70ddf974
Lionel Mann <lionelmann@gmail.com>
2017-03-27 14:28:20 -0400

    facebook link added

--- custom/themes/lori/footer.php
+++ custom/themes/lori/footer.php
@@ -8,7 +8,8 @@
         PO Box 76072 Abbey Market<br>
         Oakville ON L6M3H5<br><br>
         <b>tel:</b> 416 648 7322<br>
-        <b>fax:</b> 289-815-0639
+        <b>fax:</b> 289-815-0639<br><br>
+        <i class="fa fa-facebook-square" aria-hidden="true"></i> <a href="https://www.facebook.com/Desjardine-Rehabilitation-276792526086062/" target="_blank">Join our Facebook page</a>
         </div>
         <div class="columns-3">
             <a href="http://www.brainfx.com/" target="_blank"><img src="<?php bloginfo('template_url' ); ?>/dist/images/brainseal_admin.png"></a>
